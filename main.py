f9ce9f7c682c1a68ef051956100ce5b229b96d5d
Dagon <dagoncal@gmx.com>
2018-08-23 22:30:43 +0100

    Add support for  multiple vaults

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
+from azure.mgmt.resource import SubscriptionClient
 from azure.keyvault import KeyVaultClient, KeyVaultAuthentication
+from azure.mgmt.keyvault import KeyVaultManagementClient
 from azure.common.credentials import ServicePrincipalCredentials
-import json, jmespath, base64
-import os, pprint
+from azure.keyvault.models.key_vault_error_py3 import KeyVaultErrorException
+from urllib.parse import urlparse
+import os, json, jmespath, base64
 
-az_client_id = ''
-az_secret = ''
-az_tenant = ''
-keyvault_uri = CRED
+az_client_id = CRED
+az_secret = CRED
+az_tenant = CRED
+#subscription_id = CRED
+#keyvault_uri = CRED
+credentials = ServicePrincipalCredentials(
+    client_id = az_client_id,
+    secret = az_secret,
+    tenant = az_tenant
+)
+
 secrets = []
+subscription_ids = []
+keyvault_list = []
 
 class bcolors:
     HEADER = '\033[95m'
@@ -22,7 +34,7 @@ class bcolors:
     BACKGROUND = '\033[100m'
     BACKGROUND_GRAY = '\033[48;5;235m'
 
-
+# Get tokens for keyvault access
 def auth_callback(server, resource, scope):
     credentials = ServicePrincipalCredentials(
         client_id = az_client_id,
@@ -33,10 +45,12 @@ def auth_callback(server, resource, scope):
     token = credentials.token
     return token['token_type'], token['access_token']
 
-# Pass the complte secret id
+# Pass the complete secret id
 def getSecret(secret_id):
-    secret_value = client.get_secret(
-        keyvault_uri,
+    url = urlparse(secret_id)
+
+    secret_value = keyvault_client.get_secret(
+        'https://' + url.netloc,
         os.path.basename(secret_id),
         ''
     )
@@ -52,15 +66,39 @@ def isBase64(s):
     except Exception:
         return False
 
-client = KeyVaultClient(KeyVaultAuthentication(auth_callback))
+# Get list of subscription id's
+print('\nChecking subscriptions...')
+subscription_client = SubscriptionClient(credentials)
+subscriptions = subscription_client.subscriptions.list()
+for item in subscriptions:
+    item = item.as_dict()
+    subscription_ids.append(item['subscription_id'])
 
-secrets_objects = client.get_secrets(keyvault_uri)
+# Get keyvaults in all subscriptions
+print('Retrieving list of keyvaults...\n')
+for item in subscription_ids:
+    kv_mgmt_client = KeyVaultManagementClient(credentials, item)
+    kv = kv_mgmt_client.vaults.list()
+    for item in kv:
+        item = item.as_dict()
+        keyvault_list.append(item['name'])        
 
-#Add all secrets to a list of dicts
-print('Loading secrets...')
-for item in (secrets_objects):
-    secrets.append(item.as_dict())
-    #print('.', end='', flush=True)
+# Get list of secrets from all kevaults
+keyvault_client = KeyVaultClient(KeyVaultAuthentication(auth_callback))
+for item in keyvault_list:
+    print('Loading secrets from ' + item + '...',  end='', flush=True)
+
+    try:
+        secrets_objects = keyvault_client.get_secrets('https://' + item + '.vault.azure.net/')
+        for item in (secrets_objects):
+            secrets.append(item.as_dict())
+        print(bcolors.GREEN + 'OK' + bcolors.RESET)
+
+    except KeyVaultErrorException as err:
+        if err.message == '(Forbidden) Access denied':
+            print(bcolors.YELLOW + err.message + bcolors.RESET)
+        else:
+            print(bcolors.RED + err.message + bcolors.RESET)
 
 print('Loaded', len(secrets), 'secrets')
 
@@ -82,7 +120,6 @@ while True:
 
     secret = getSecret(search_result[user_input - 1])
     print(bcolors.GREEN + secret + bcolors.RESET)
-
 
     if isBase64(secret):
         print("\nThis secret is base64 encoded. Here's the decoded version:")
