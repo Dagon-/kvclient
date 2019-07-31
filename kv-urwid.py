87762f2e5bf857d04e19116cda81667bb00377e2
Dagon <dagoncal@gmx.com>
2019-07-31 19:07:12 +0100

    Inital loading tests

--- kv-urwid.py
+++ kv-urwid.py
@@ -1,3 +1,11 @@
+from azure.mgmt.resource import SubscriptionClient
+from azure.keyvault import KeyVaultClient, KeyVaultAuthentication
+from azure.mgmt.keyvault import KeyVaultManagementClient
+from azure.common.credentials import ServicePrincipalCredentials
+from azure.keyvault.models.key_vault_error_py3 import KeyVaultErrorException
+from multiprocessing import Pool
+from multiprocessing.dummy import Pool as ThreadPool
+from urllib.parse import urlparse
 import os
 import sys
 import json
@@ -7,7 +15,20 @@ import jmespath
 import collections
 
 from custom_widgets import MyButton
+#from azurecalls import get_secrets 
 
+class bcolors():
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[0;93m'
+    WARNING = '\033[93m'
+    RED = '\033[91m'
+    RESET = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    BACKGROUND = '\033[100m'
+    BACKGROUND_GRAY = '\033[48;5;235m'
 
 class kvDisplay():
 
@@ -17,15 +38,11 @@ class kvDisplay():
         ('highlight', 'white', 'dark gray')
     ]
 
-    choices = [ u"test2", u"test4",  u"test5",
-                u"test7", u"test8",  u"test9"
-              ]
-
     def __init__(self, output_mode='result'):
         self.view = None
         self.output_mode = output_mode
         self.last_result = None
- 
+
     def _get_font_instance(self):
         return urwid.get_all_fonts()[-2][1]()
 
@@ -66,7 +83,7 @@ class kvDisplay():
 
         div = urwid.Divider()
         self.header = urwid.Pile([self.status_bar, div,
-             urwid.AttrMap(self.input_expr, 'input expr'), div],
+            urwid.AttrMap(self.input_expr, 'input expr'), div],
             focus_item=2)
 
 
@@ -115,6 +132,7 @@ class kvDisplay():
 
             
 
+
 def main():
 
     screen = urwid.raw_display.Screen()
@@ -122,5 +140,136 @@ def main():
     display.main(screen=screen)
 
 
+
+
+
+
+
+
+
+
+#############################
+
+
+
+
+subscription_ids = []
+keyvault_list = []
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--clientid", help="Service principle client ID")
+parser.add_argument("--secret", help="Service principle client secret")
+parser.add_argument("--tenant", help="AD tenent ID")
+parser.add_argument("--credsfile", help="json file with keyvault credentials")
+args = parser.parse_args()
+
+# Get credentails 
+# TODO This works but it not very readble
+# Rework with a cleaner flow.
+if not all((args.clientid, args.secret, args.tenant)):
+    if args.credsfile and os.path.isfile(args.credsfile):
+        creds_file = args.credsfile
+    elif 'AZURE_AUTH_LOCATION' in os.environ:
+        creds_file = os.environ['AZURE_AUTH_LOCATION']
+    elif 'AZURE_AUTH_LOCATION' not in os.environ:
+        homedir = os.path.expanduser("~")
+        if os.path.isfile(homedir + '/.azure/keyvault.json'):
+            creds_file = homedir + '/.azure/keyvault.json'
+        else:
+            print('\nNo credentials found. See readme.md for options.\n')
+            sys.exit()
+
+    with open(creds_file) as json_data:
+        creds = json.load(json_data)
+
+    az_client_id = creds['clientId']
+    az_secret = creds['clientSecret']
+    az_tenant = creds['tenantId']
+else:
+    if args.credsfile is not None:
+        print('\n--credsfile cannot be used at the same time as other credential agruments.\n')
+        sys.exit()
+
+    az_client_id = args.clientid
+    az_secret = args.secret
+    az_tenant = args.tenant
+
+credentials = ServicePrincipalCredentials(
+    client_id = az_client_id,
+    secret = az_secret,
+    tenant = az_tenant
+)
+
+# Get tokens for keyvault access
+def auth_callback(server, resource, scope):
+    credentials = ServicePrincipalCredentials(
+        client_id = az_client_id,
+        secret = az_secret,
+        tenant = az_tenant,
+        resource = "https://vault.azure.net"
+    )
+    token = credentials.token
+    return token['token_type'], token['access_token']
+
+# Get a list of secrets from a keyvault
+def list_secrets(keyvault_list):
+    secrets = []
+    
+    try:
+        secrets_objects = keyvault_client.get_secrets(
+            'https://{}.vault.azure.net/'.format(keyvault_list)
+            )
+
+        for item in (secrets_objects):
+            secrets.append(item.as_dict())
+
+        print('Loading secrets from {:.<25}'.format(keyvault_list),  end='', flush=True)
+        print(bcolors.GREEN + 'OK' + bcolors.RESET)
+
+    except KeyVaultErrorException as err:
+        if err.message == '(Forbidden) Access denied':
+            print('Loading secrets from {:.<25}'.format(keyvault_list),  end='', flush=True)
+            print(bcolors.YELLOW + err.message + bcolors.RESET)
+        elif 'is not authorized and caller is not a trusted service' in err.message:
+            print('Loading secrets from {:.<25}'.format(keyvault_list),  end='', flush=True)
+            print(bcolors.YELLOW + err.message + bcolors.RESET)
+        else:
+            print('Loading secrets from {:.<25}'.format(keyvault_list),  end='', flush=True)
+            print(bcolors.RED + err.message + bcolors.RESET)
+
+    return secrets
+
+# Get list of subscription id's
+print('\nChecking subscriptions.........', end='', flush=True)
+subscription_client = SubscriptionClient(credentials)
+subscriptions = subscription_client.subscriptions.list()
+for item in subscriptions:
+    item = item.as_dict()
+    subscription_ids.append(item['subscription_id'])
+print(bcolors.GREEN + 'OK' + bcolors.RESET)
+
+
+# Get keyvaults in all subscriptions
+print('Retrieving list of keyvaults...', end='', flush=True)
+for item in subscription_ids:
+    kv_mgmt_client = KeyVaultManagementClient(credentials, item)
+    kv = kv_mgmt_client.vaults.list()
+    for item in kv:
+        item = item.as_dict()
+        keyvault_list.append(item['name'])
+print(bcolors.GREEN + 'OK\n' + bcolors.RESET)
+
+
+# Get list of secrets from all kevaults in parallel
+keyvault_client = KeyVaultClient(KeyVaultAuthentication(auth_callback))
+pool = ThreadPool()
+s =  pool.map(list_secrets, keyvault_list)
+pool.close()
+pool.join()
+# flatten the list of lists returned by pool.map
+master_list = [item for sublist in s for item in sublist]
+print('Loaded', len(master_list), 'secrets')
+
+
 if __name__ == '__main__':
-    sys.exit(main())
+   sys.exit(main())
